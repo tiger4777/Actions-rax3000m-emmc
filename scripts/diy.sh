ddb091e8644b1d233e106ca65adac84f8dad6d5c
tiger4777 <263050007@qq.com>
2024-09-06 08:27:21 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,4 +2,5 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
+#定时设置
+git clone https://git.homegu.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
